8bf86ee946f114d126d3414d0bce69d353bb7f30
Stella Flores <floresstella2015@gmail.com>
2022-08-20 21:05:43 -0300

    Nav agregado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <header className="navBar">
+      <h2>Buy Smart</h2>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Prices</li>
+          <li>Contact</li>
+          </ul>
+      </nav>
+      <div className="btn">
+        <button>Login</button>
+        </div>
+          
       </header>
-    </div>
   );
 }
-
 export default App;
+
