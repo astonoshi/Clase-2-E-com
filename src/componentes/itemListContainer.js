42f727b968996892998eb28129241f5273673576
astonoshi <100243193+astonoshi@users.noreply.github.com>
2022-09-03 19:11:01 -0300

    Update itemListContainer.js

--- src/componentes/itemListContainer.js
+++ src/componentes/itemListContainer.js
@@ -1,7 +1,7 @@
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import ItemList from "./ItemList"
-import CustomFetch from "../utils/CustomFetch";
+import promise from "../utils/promise";
 import { data } from "../utils/data"
 import { useEffect, useState } from "react";
 
@@ -9,7 +9,7 @@ const ItemListContainer = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        CustomFetch(data)
+        promise(data)
             .then(res => setProducts(res))
             .catch(err => console.log(err))
     }, []);
